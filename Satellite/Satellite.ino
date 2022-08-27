ab0d83b9790bdd6d05fcc1aebe92fdfeeedad36f
TSprech <trey.sprecher@gmail.com>
2022-08-27 17:08:10 -0400

    Satellite Multithreading
    FreeRTOS threads are now running the satellite to make the tap reads separate from the data sending.

--- Satellite/Satellite.ino
+++ Satellite/Satellite.ino
@@ -4,6 +4,7 @@
 #include <ArduinoJson.h>
 #include <Embedded_Template_Library.h>
 #include <etl/array.h>
+#include <etl/atomic.h>
 #include <Wire.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
@@ -13,6 +14,16 @@ Adafruit_LIS3DH lis;
 
 #define CLICKTHRESHHOLD 180
 
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#define OTHER_CORE 1
+#else
+#define ARDUINO_RUNNING_CORE 1
+#define OTHER_CORE 0
+#endif
+
+TaskHandle_t task_check_taps;
+TaskHandle_t task_send_data;
 
 using mac_t = etl::array<uint8_t, 6>;
 
@@ -24,7 +35,45 @@ struct Hub {
 
 Hub hub = { { 0xF4, 0x12, 0xFA, 0x59, 0x6A, 0x64 } };
 
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+etl::atomic<uint16_t> number_of_taps;
+
+void TaskCheckTaps(void* pvParameters) {
+  (void) pvParameters;
+  for (;;) {
+    auto click = lis.getClick();
+    if (click & 0x10 && !(click == 0) && (click & 0x30)) ++number_of_taps;
+    delay(20);
+  }
+}
+
+void TaskSendData(void* pvParameters) {
+  (void) pvParameters;
+  for (;;) {
+  StaticJsonDocument<100> testDocument;
+
+  testDocument["globalReset"] = false;
+  testDocument["localReset"] = true;
+  testDocument["threshold"] = number_of_taps.load();
+
+  uint8_t buffer[100];
+
+  int bytesWritten = serializeMsgPack(testDocument, buffer);
+  // for(int i = 0; i<bytesWritten; i++){
+  //   Serial.printf("%02X ",buffer[i]);
+  // }
+  Serial.println("Finished serializing");
+  esp_err_t result = esp_now_send(hub.mac_address.data(), buffer, bytesWritten);
+  if (result == ESP_OK)
+    Serial.println("Sent data successfully!");
+  else {
+    Serial.print("Error sending the data: ");
+    Serial.println(result);
+  }
+  delay(200);
+  }
+}
+
+void OnDataSent(const uint8_t* mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
@@ -32,19 +81,12 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 void setup() {
   //Initialize Serial Monitor
   Serial.begin(115200);
-  while (!Serial)
-    ;
   Debug.setDebugOutputStream(&Serial);
   Debug.setDebugLevel(DBG_VERBOSE);
   Debug.newlineOff();
-  Serial.println("Test");
-  Debug.print(DBG_VERBOSE, "DBG Test\n");
 
   //Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
-
-  pinMode(7, OUTPUT);
-  digitalWrite(7, HIGH);
 
   //Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
@@ -52,8 +94,6 @@ void setup() {
     return;
   }
 
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
   esp_now_register_send_cb(OnDataSent);
   // esp_now_register_recv_cb(OnDataRecv);
 
@@ -74,35 +114,25 @@ void setup() {
   lis.begin(0x18);
   lis.setRange(LIS3DH_RANGE_4_G);  // 2, 4, 8 or 16 G!
   lis.setClick(1, CLICKTHRESHHOLD);
+
+  xTaskCreatePinnedToCore(
+    TaskCheckTaps,
+    "TaskCheckTaps",
+    10000,
+    NULL,
+    2,
+    &task_check_taps,
+    OTHER_CORE);
+
+  xTaskCreatePinnedToCore(
+    TaskSendData,
+    "TaskSendData",
+    10000,
+    NULL,
+    3,
+    &task_send_data,
+    ARDUINO_RUNNING_CORE);
 }
 
-int thres = 0;
-
 void loop() {
-  StaticJsonDocument<100> testDocument;
-
-  testDocument["globalReset"] = false;
-  testDocument["localReset"] = true;
-  testDocument["threshold"] = thres;
-
-  uint8_t buffer[100];
-
-  int bytesWritten = serializeMsgPack(testDocument, buffer);
-  // for(int i = 0; i<bytesWritten; i++){
-  //   Serial.printf("%02X ",buffer[i]);
-  // }
-  Serial.println("Finished serializing");
-  esp_err_t result = esp_now_send(hub.mac_address.data(), buffer, bytesWritten);
-  if (result == ESP_OK)
-    Serial.println("Sent data successfully!");
-  else {
-    Serial.print("Error sending the data: ");
-    Serial.println(result);
-  }
-
-  uint8_t click = lis.getClick();
-  if (click & 0x10 && !(click == 0) && (click & 0x30)) {
-    ++thres;
-  }
-  delay(200);
 }
